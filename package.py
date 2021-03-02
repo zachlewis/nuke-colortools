f84c097d68f9a546d36c0af93ce72d913068254d
Zach Lewis <zachcanbereached@gmail.com>
2021-03-01 20:04:31 -0500

    add rez package definition

---
+++ package.py
@@ -0,0 +1,26 @@
+name = 'nuke_colourtools'
+
+version='1.0.3'
+
+authors = ['jed smith']
+
+help = 'github.com/jedypod'
+
+requires = [
+    'nuke-9+',
+]
+
+def commands():
+    env.NUKE_PATH.append('{root}/nuke')
+    env.PYTHONPATH.append('{root}/nuke/Python')
+    env.NUKE_COLOURTOOLS_IMAGES.set('{root}/images')
+
+build_command = """
+prefix=$REZ_BUILD_INSTALL_PATH/nuke
+mkdir -p $prefix
+cp -a {root}/blink $prefix
+cp -a {root}/python $prefix/Python
+cp -a {root}/toolsets $prefix/ToolSets
+cp -a {root}/images $prefix
+cp {root}/*.md $prefix
+"""
